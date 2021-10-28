e6604a30933139fd7ae044a7132f2a0fc2b7a5ee
Филоненко Александр <alcohealer@gmail.com>
2021-10-28 23:41:31 +0300

    Complete Ex05

---
+++ Example05_ConditionElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "джон")
+{
+    Console.Write("Yippee ki-yay, ");
+    Console.WriteLine(username);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
